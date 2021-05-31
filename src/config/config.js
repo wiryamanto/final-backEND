c6b791c9b2e54ddbe66ba65f4d8e22f2bb24a5aa
wiryamanto <wiryamantoragil@gmail.com>
2021-05-31 19:19:21 +0700

    add procfile

--- src/config/config.js
+++ src/config/config.js
@@ -7,11 +7,22 @@ module.exports={
     "dialect": "mysql"
   },
   
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "ragil_final",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // }
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "ragil_final",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    use_env_variable: 'DATABASE_URL',
+    dialect: "postgress",
+    protocol: "postgress",
+    dialectOptions: {
+      ssl : {
+        require : true,
+        rejectUnauthorized : false
+      }
+    }
   }
 }
